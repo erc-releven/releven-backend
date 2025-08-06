e29507f7cfb742159263e38d165af76142c7d3fd
Kevin Stadler <kevinstadler@users.noreply.github.com>
2025-08-06 11:50:00 +0200

    feat: add filter options to /people and /text listing

--- generate-endpoints.sh
+++ generate-endpoints.sh
@@ -5,7 +5,7 @@
 # remove semantic g_/p_ prefixes for nicer automatically generate class and endpoint names
 sed -e 's/id>[gp]_/id>/g' "pathbuilder_wisski.xml" > tmp.xml
 
-uv run wisskas -v tmp.xml endpoints --git \
+uv run wisskas tmp.xml endpoints --git \
   -a "https://graphdb.r11.eu/repositories/RELEVEN_2025" \
   -p aaao "https://ontology.swissartresearch.net/aaao/" \
   -p crm "http://www.cidoc-crm.org/cidoc-crm/" \
@@ -25,7 +25,7 @@ uv run wisskas -v tmp.xml endpoints --git \
   -li language '*' \
   -li legal_status '*' \
   -li passage '*' \
-  -li person/people 'person_display_name' 'person_id_assignment.*' 'person_id_assignment.person_id_assignment_identifier.*' 'person_id_assignment.person_id_assignment_by.external_authority_display_name' 'person_name_of_person_assertion.person_name_of_person_is' \
+  -li person/people\|person_display_name 'person_display_name' 'person_id_assignment.*' 'person_id_assignment.person_id_assignment_identifier.*' 'person_id_assignment.person_id_assignment_by.external_authority_display_name' 'person_name_of_person_assertion.person_name_of_person_is' \
   -ii person/people/detail id 'person_display_name' '*' 'person_gender_assignment.person_gender_assignment_gender_assertion.person_gender_assignment_gender_is.gender_display_name' 'person_id_assignment.*' 'person_id_assignment.person_id_assignment_identifier.*' 'person_name_of_person_assertion.*' 'person_name_of_person_assertion.person_name_of_person_by.*' 'person_name_of_person_assertion.person_name_of_person_src.*' \
   -li place '*' \
   -li publication '*' \
@@ -33,6 +33,6 @@ uv run wisskas -v tmp.xml endpoints --git \
   -li seal '*' \
   -li seal_collection '*' \
   -li social_relationship '*' \
-  -li written_text/text '*' 'written_text_creation.*' \
+  -li written_text/text\|written_text_display_name '*' 'written_text_creation.*' \
   -ii written_text/text/detail id '*' 'written_text_creation.*' 'written_text_creation.written_text_creation_author_assertion.*' 'written_text_creation.written_text_creation_author_assertion.written_text_creation_author_is.*' 'written_text_creation.written_text_creation_author_assertion.written_text_creation_author_by.*' 'written_text_creation.written_text_creation_author_assertion.written_text_creation_author_src.*' \
   -o releven || exit 1

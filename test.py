3ae6ce19fe3e8a95353b5cb621d9ffd93cc75e01
Kevin Stadler <kevinstadler@users.noreply.github.com>
2025-10-20 14:38:09 +0200

    test: skip tests which require network

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from releven import app
@@ -5,6 +6,12 @@ from releven import app
 client = TestClient(app)
 
 
+def test_read_debug():
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+@pytest.mark.skip(reason="might fail due to network timeouts")
 def test_read_counts():
     response = client.get("/counts")
     assert response.status_code == 200
